704f51bf7b2541a26ec3e8c08b41880fb4867207
Kitty <andrianinaminou@gmail.com>
2018-11-08 20:41:44 +0300

    nanamp  getters ao musique

--- src/model/Musique.java
+++ src/model/Musique.java
@@ -4,4 +4,21 @@ import dao.BaseModele;
 
 public class Musique extends BaseModele{
     private String titre;
+    private String idartiste;
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getIdartiste() {
+        return idartiste;
+    }
+
+    public void setIdartiste(String idartiste) {
+        this.idartiste = idartiste;
+    }
 }
