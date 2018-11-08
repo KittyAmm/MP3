c76517598ce73dc9ee77b64c8f864be7abb7244a
Kitty <andrianinaminou@gmail.com>
2018-11-08 19:53:43 +0300

    nanamp getters

--- src/model/Personne.java
+++ src/model/Personne.java
@@ -22,4 +22,8 @@ public class Personne extends BaseModele {
                 ", prenom='" + prenom + '\'' +
                 '}';
     }
+
+    public String getPrenom() {
+        return prenom;
+    }
 }
