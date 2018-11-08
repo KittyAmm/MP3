1e599c286564a9a00eebb26174ed0083c1a93563
KittyAmm <44875882+KittyAmm@users.noreply.github.com>
2018-11-08 20:44:58 +0300

    Update Musique.java

--- src/model/Musique.java
+++ src/model/Musique.java
@@ -25,4 +25,7 @@ public class Musique extends BaseModele{
     public void setIdartiste(String idartiste) {
         this.idartiste = idartiste;
     }
+    public void getIdgenre(){
+        return idgenre;
+    }
 }
