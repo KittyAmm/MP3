0c2ab70e6da46692145a6aa8a2e413a14742f600
Kitty <andrianinaminou@gmail.com>
2018-11-22 22:25:17 +0300

    projet nouveau depart

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
 
 public class Main {
     public static void main(String[] arg) throws Exception {
-      /* GenericDao gdao = new GenericDao();
-        Personne  p    = new Personne("malala", "minou");
-        Produit   p1   = new Produit("kapa","23/07/1998",1000);
-        Produit   p2   = new Produit("kapa","23/07/1998",1000);
-        Produit   p3   = new Produit("kapa","23/07/1998",1000);
-        Produit   p4   = new Produit("kapa","23/07/1998",1000);
-        Produit   p5   = new Produit("kapa","23/07/1998",1000);
-        Produit   p6   = new Produit("kapa","23/07/1998",1000);
-        Produit   p7   = new Produit("kapa","23/07/1998",1000);
-        Produit   p8   = new Produit("kapa","23/07/1998",1000);
-        Produit p9 = new Produit("kapa","23/07/1998",1000);
-        Produit p10 = new Produit("kapa","23/07/1998",1000);
-        p.setId("6");
-        gdao.update(p);
-        gdao.save(p2);
+//       GenericDao gdao = new GenericDao();
+//        Personne  p    = new Personne("malala", "minou");
+//        Produit   p1   = new Produit("kapa","23/07/1998",1000);
+//        Produit   p2   = new Produit("kapa","23/07/1998",1000);
+//        Produit   p3   = new Produit("kapa","23/07/1998",1000);
+//        Produit   p4   = new Produit("kapa","23/07/1998",1000);
+//        Produit   p5   = new Produit("kapa","23/07/1998",1000);
+//        Produit   p6   = new Produit("kapa","23/07/1998",1000);
+//        Produit   p7   = new Produit("kapa","23/07/1998",1000);
+//        Produit   p8   = new Produit("kapa","23/07/1998",1000);
+//        Produit p9 = new Produit("kapa","23/07/1998",1000);
+//        Produit p10 = new Produit("kapa","23/07/1998",1000);
+//        p.setId("6");
+//        gdao.update(p);
+        /*gdao.save(p2);
         gdao.save(p3);
         gdao.save(p4);
         gdao.save(p5);
@@ -23,12 +23,12 @@ public class Main {
         gdao.save(p7);
         gdao.save(p8);
         gdao.save(p9);
-        gdao.save(p10);
-       System.out.println(new GenericDao().queryPagination(new Produit(),3,5,5));
-        List<BaseModele> ba = gdao.findAll(new Produit(),5,3,5);
-        for(int i = 0; i < ba.size(); i++){
-            Produit prod = (Produit)ba.get(i);
-            System.out.println(String.valueOf(prod.getId()));
-        }*/
+        gdao.save(p10);*/
+//       System.out.println(new GenericDao().queryPagination(new Produit(),3,5,5));
+//        List<BaseModele> ba = gdao.findAll(new Produit(),5,3,5);
+//        for(int i = 0; i < ba.size(); i++){
+//            Produit prod = (Produit)ba.get(i);
+//            System.out.println(String.valueOf(prod.getId()));
+//        }
     }
 }
