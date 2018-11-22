f4fddc4ce070407d7e2aec989dee9f739dc4e424
unknown <hajafaniry@hotmail.com>
2018-11-22 20:51:43 +0300

    Ajout titre

---
+++ src/modelemp3master/Album.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelemp3master;
+
+/**
+ *
+ * @author Haja Faniry
+ */
+public class Album extends BaseModele  {
+    
+    String idalbum;
+    String idartiste;
+    String idimage;
+    String nom;
+
+    public void setIdalbum(String idalbum) {
+        this.idalbum = idalbum;
+    }
+
+    public void setIdartiste(String idartiste) {
+        this.idartiste = idartiste;
+    }
+
+    public void setIdimage(String idimage) {
+        this.idimage = idimage;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getIdalbum() {
+        return idalbum;
+    }
+
+    public String getIdartiste() {
+        return idartiste;
+    }
+
+    public String getIdimage() {
+        return idimage;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+    
+    
+}
---
+++ src/modelemp3master/Image.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelemp3master;
+
+/**
+ *
+ * @author Haja Faniry
+ */
+public class Image extends BaseModele {
+    String idimage;
+    String nom;
+
+    public String getIdimage() {
+        return idimage;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setIdimage(String idimage) {
+        this.idimage = idimage;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    
+    
+}
